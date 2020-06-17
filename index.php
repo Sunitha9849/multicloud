3b6004cb37846374f1811c692d25bbeacf9709fb
Sunitha9849 <66708814+Sunitha9849@users.noreply.github.com>
2020-06-17 16:02:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <h2> Thank You </h2>
 <?php
    $cloudant_url=`head -n1 path.txt`;
-   $img_path="https://".$cloudant_url."/terraform.png";
+   $img_path="https://".$cloudant_url."/terraform.jpg";
    echo "<br>";
    echo "<img src='${img_path}' width=100 height=100>";
 ?>
